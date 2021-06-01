c52d6ff5c2445d23a689b340e064753659b7717b
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-06-01 23:36:04 +0900

    Feat promotion section toggle

--- js/main.js
+++ js/main.js
@@ -76,3 +76,18 @@ new Swiper(".promotion .swiper-container", {
     nextEl: ".promotion .swiper-next",
   },
 });
+
+const promotionEl = document.querySelector(".promotion");
+const pormotrionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
+pormotrionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove("hide");
+  }
+});
